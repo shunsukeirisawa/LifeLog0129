37cfd5828fdb5bd8058b58d6a0211587c1d96834
shunsukeirisawa <shunsuke.irisawa@gmail.com>
2022-01-29 16:50:01 +0900

    Initial Commit

---
+++ LifeLog0129/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LifeLog0129
+//
+//  Created by 入澤俊介 on 2022/01/29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ LifeLog0129Tests/LifeLog0129Tests.swift
@@ -0,0 +1,33 @@
+//
+//  LifeLog0129Tests.swift
+//  LifeLog0129Tests
+//
+//  Created by 入澤俊介 on 2022/01/29.
+//
+
+import XCTest
+@testable import LifeLog0129
+
+class LifeLog0129Tests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
